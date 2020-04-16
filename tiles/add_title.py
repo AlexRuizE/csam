876bbf6bd0178584e67f70c50c09363ab989759d
Steve Brown <steve@stevejbrown.com>
2020-04-15 20:08:59 -0700

    Add adaptive text sizing
    This adds the ability for text to shrink/expand to fill the height given to the draw_text function. It may still under or overflow the width.

--- tiles/add_title.py
+++ tiles/add_title.py
@@ -1,7 +1,8 @@
+from math import floor
 from typing import Tuple
 
 import click
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 
 @click.command()
@@ -18,14 +19,13 @@ def add_title(source_image: str):
     orig_img = Image.open(source_image)
     orig_width, orig_height = orig_img.size
 
-    title = draw_text("text", (orig_width, 18))
+    title = draw_text("text", (orig_width, floor(orig_height*0.1)))
     _, title_height = title.size
 
     image = Image.new("RGBA", (orig_width, orig_height + title_height))
     image.paste(title, (0, 0))
     image.paste(orig_img, (0, title_height))
     image.show()
-
 
 def draw_text(text: str, image_size: Tuple[int, int]):
     """Draws a text image
@@ -38,10 +38,23 @@ def draw_text(text: str, image_size: Tuple[int, int]):
         PIL.Image: An image of text
     """
     image = Image.new("RGBA", image_size, color="blue")
+    point_size = pixels_to_points(image_size[1])
+    # Load font to use
+    font = ImageFont.truetype('Arial.ttf', floor(point_size))
     draw_obj = ImageDraw.Draw(image)
-    draw_obj.text((0,0), text)
+    draw_obj.text((0,0), text, font=font)
     return image
 
+def pixels_to_points(pixels):
+    """Convert pixels to points assuming 96 dots per inch
+
+    Args:
+        pixels (int): Number of pixels to convert to point
+
+    Returns:
+        float: Number of points equivalent
+    """
+    return pixels * 72/96
 
 if __name__ == '__main__':
     add_title()
