a81d8f189319564a8d4cd08b4c621bce029b25fb
Paul Singman <paul.singman@equinox.com>
2020-06-10 14:25:05 -0400

    default docker paths

--- detection/tile_detector.py
+++ detection/tile_detector.py
@@ -95,11 +95,13 @@ if __name__ == '__main__':
                         help='Path to model parameters.')
     parser.add_argument('-i', '--idir', dest='image_dir',
                         type=str,
-                        required=True,
+                        required=False,
+                        default='/var/local'
                         help='Path to images for classification.')
     parser.add_argument('-o', '--odir', dest='out_dir',
                         type=str,
-                        required=True,
+                        required=False,
+                        default='/var/local/output'
                         help='Output directory for detected tile image paths.')
     parser.add_argument('-f', '--mfile', dest='model_file',
                         type=str,
